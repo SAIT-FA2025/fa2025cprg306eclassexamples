e4da948cf1dee0737982c364bae9e18b344b39c1
Derek <derek.yadlowski@sait.ca>
2025-09-16 11:50:49 -0600

    Week 2 Examples

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import MyFirstComponent from "../_components/my-first-component";
+export default function MySecondPage(){
+    return(
+        <main>
+            <h1>My second page</h1>
+            <p>This is my second page of my new react web app!</p>
+            <MyFirstComponent />
+        </main>
+    );
+} 
